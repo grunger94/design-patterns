071e766836ff579bc78e6772584ae77b873ee8de
Agustin Santana <agustin.santana@gmail.com>
2018-07-03 10:19:47 -0500

    Hotfix properties

--- guti/strategy/Leche.php
+++ guti/strategy/Leche.php
@@ -4,8 +4,8 @@ require_once "Beberage.php";
 class Leche extends Beberage {
     
     public function __construct() {
-        $this->limiteEdad = 12;
-        $this->nombreBebida ="Refresco";
+        $this->limiteEdad = 0;
+        $this->nombreBebida ="Leche";
     }    
 }
 ?>
