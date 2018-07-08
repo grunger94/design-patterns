dc9e779bc4905b754fe2cc5d31807e1b59581915
Hugo Pedro (MacBook FD) <pedhde@gmail.com>
2018-07-08 12:37:02 -0500

    Create abstract class Operacion

---
+++ src/template/pedro/Operacion.java
@@ -0,0 +1,47 @@
+package template.pedro;
+
+abstract class Operacion {
+    protected double primerValor;
+    protected double segundoValor;
+    protected double resultado;
+    protected Integer redondeo = null;
+
+    final void ejecuta(double primerValor, double segundoValor) {
+        getOperacion();
+        setValores(primerValor, segundoValor);
+        calcula();
+
+        /*if (redondeo != null && redondeo >= 0) {
+            redondea();
+        }*/
+
+        muestraResultado();
+    }
+
+    abstract void getOperacion();
+    abstract void calcula();
+
+    final void setValores(double primerValor, double segundoValor) {
+        this.primerValor = primerValor;
+        this.segundoValor = segundoValor;
+        System.out.println("primer valor: " + this.primerValor);
+        System.out.println("segundo valor: " + this.segundoValor);
+    }
+
+    final void muestraResultado() {
+        if (redondeo != null && redondeo >= 0) {
+            System.out.println("redondeo a " + redondeo + " decimales");
+            System.out.println("resultado: " + String.format("%." + redondeo + "f", resultado));
+        } else {
+            System.out.println("sin redondear");
+            System.out.println("resultado: " + resultado);
+        }
+
+        System.out.println("\n");
+    }
+
+    final void redondea() {
+        double factor = Math.pow(10, redondeo);
+        resultado = (double) Math.round(resultado * factor) / factor;
+    }
+}
